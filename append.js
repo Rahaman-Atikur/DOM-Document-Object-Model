f5e7d33ca3a09b6c71a660112b8119a98e6ac020
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-28 01:10:23 +0100

    Done with everything

---
+++ append.js
@@ -0,0 +1,14 @@
+const placeList = document.getElementById('places-list');
+const li = document.createElement('li');
+li.innerText = 'West Wick';
+placeList.appendChild(li);
+
+// Creating a section in JavaScript
+const section = document.createElement('section');
+const h1 = document.createElement('h1');
+h1.innerText = 'My Food';
+section.appendChild(h1);
+const ul = document.createElement('ul');
+const li1 = document.createElement('li');
+li1.innerText='Biriyani';
+ul.appendChild(li1); 
